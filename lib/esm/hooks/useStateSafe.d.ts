274d6cc79dad8a180e2c8b75c92e23b06ab13b86
Alex <dr.piou@gmail.com>
2022-06-16 00:12:40 +0200

    fix useStateSafe type

--- lib/esm/hooks/useStateSafe.d.ts
+++ lib/esm/hooks/useStateSafe.d.ts
@@ -1,4 +1,4 @@
 import React from 'react';
 export declare type UseStateSafe<T> = [T, React.Dispatch<React.SetStateAction<T>>];
-declare const useStateSafe: <T>(initialValue: T) => UseStateSafe<T>;
+declare const useStateSafe: <T>(initialValue: T | (() => T)) => UseStateSafe<T>;
 export default useStateSafe;
