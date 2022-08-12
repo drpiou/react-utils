c7caa54c29f2bd2c6b071ab1102e2b423ee014d2
Alex <dr.piou@gmail.com>
2022-08-12 02:14:33 +0200

    using vite

--- lib/hoc/withHooks.tsx
+++ lib/hoc/withHooks.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import castArray from 'lodash/castArray';
 import reduce from 'lodash/reduce';
 import React, { ComponentType } from 'react';
